07a16dadac42e5a4b93fd7225d28d21465495eab
Jack Dickson <98648241+JackOfDickson@users.noreply.github.com>
2023-08-11 11:12:34 +0100

    Setting up the user model and services to connect to mongodb

---
+++ webapi/Services/UserService.cs
@@ -0,0 +1,19 @@
+﻿using webapi.Models;
+using MongoDB.Driver;
+using System.Runtime.CompilerServices;
+using Microsoft.Extensions.Options;
+
+namespace webapi.Services
+{
+    public class UserService
+    {
+        private readonly IMongoCollection<User> _userCollection;
+
+        public UserService(IOptions<GamerFinderDatabaseSettings> databaseSettings)
+        {
+            var mongoClient = new MongoClient(databaseSettings.Value.ConnectionString);
+            var mongoDatabase = mongoClient.GetDatabase(databaseSettings.Value.DatabaseName);
+            _userCollection = mongoDatabase.GetCollection<User>(databaseSettings.Value.UserCollectionName);
+        }
+    }
+}
