edc6418d8f5c4ce251f24cd4195f7b6ec7d9e586
Jack Dickson <98648241+JackOfDickson@users.noreply.github.com>
2023-08-13 17:38:59 +0100

    created a basic form for a new user

---
+++ reactapp/src/containers/NewUserForm.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+
+const NewUserForm = () => {
+
+    const [inputUsername, setInputUsername] = useState('')
+    const [inputEmail, setInputEmail] = useState('')
+    const [inputPassword, setInputPassword] = useState('')
+
+    return (
+    <div>
+        <p>I'm a user form</p>
+            <form>
+                <label htmlFor='input-username'>Username:</label>
+                <input type='text' id='input-username' value={inputUsername} onChange={(event) => setInputUsername(event.target.value)} required />
+                <br/>
+                <label htmlFor='input-email'>Email:</label>
+                <input type='text' id='input-email' value={inputEmail} onChange={(event) => setInputEmail(event.target.value)} required />
+                <br/>
+                <label htmlFor='input-password'>Password:</label>
+                <input type='text' id='input-password' value={inputPassword} onChange={(event) => setInputPassword(event.target.value)} required />
+            </form>
+
+
+     </div>
+    )
+}
+
+export default NewUserForm;
