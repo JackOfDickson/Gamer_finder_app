8ab4fd73ae7635e5499b9a225635ba721332f003
Jack Dickson <jackowendickson@gmail.com>
2023-08-28 14:14:01 +0100

    fixed so we can finally get some json from the db

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -1,4 +1,5 @@
 using webapi.Models;
+using webapi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<GamerFinderDatabaseSettings>(
     builder.Configuration.GetSection("GamerFinderDatabase"));
 
+builder.Services.AddSingleton<UserService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
