97496ac5575d054cb98ffca905fa0626731aa9b0
Jack Dickson <jackowendickson@gmail.com>
2023-09-14 15:26:11 +0100

    fixed some typos and added a model for a login

--- webapi/Controllers/UserController.cs
+++ webapi/Controllers/UserController.cs
@@ -14,6 +14,25 @@ namespace webapi.Controllers
 
         [HttpGet]
         public async Task<List<User>> Get() => await _userService.GetUsers();
+
+        [HttpGet("{id:length(24)}")]
+        public async Task<ActionResult<User>> get(string id)
+        {
+            var user = await _userService.GetUser(id);
+
+            if (user is null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        [HttpPost("{id:length(24)}/login")]
+        public async Task<IActionResult> LoginUser(string userId, [FromBody] LoginRequest loginRequest )
+        {
+
+        }
     }
 
 }
